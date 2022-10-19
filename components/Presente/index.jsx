b7550bec54f50c77630defe2fefed47a0d3f79fe
Bruno Sajermann <sajermannbruno@gmail.com>
2022-10-19 10:41:24 -0300

    add present component

---
+++ components/Presente/index.jsx
@@ -0,0 +1,21 @@
+import styles from './index.module.css'
+
+export default function Presente(){
+  return(
+    <div className={styles.container}>
+      <div className={styles.top}>
+      
+      </div>
+      <div className={styles.main}>
+
+      </div>
+
+      <div className={styles.tie1}>
+
+</div>
+<div className={styles.tie2}>
+
+</div>
+    </div>
+  )
+}
