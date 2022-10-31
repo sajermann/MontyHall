5b4925d30a10f0e84154a51560c010f8966c423b
Bruno Sajermann <sajermannbruno@gmail.com>
2022-10-31 08:13:31 -0300

    lidando com array de portas completo

--- functions/door.ts
+++ functions/door.ts
@@ -1,9 +1,21 @@
 import DoorModel from "../model/DoorModel";
 
-export function createDoors(quantity: number, selected: number): DoorModel[]{
-  return Array.from({length: quantity}, (_,i)=> {
+export function createDoors(quantity: number, selected: number): DoorModel[] {
+  return Array.from({ length: quantity }, (_, i) => {
     const numberTem = i + 1;
-    const hasPresent = numberTem === selected
-    return new DoorModel(numberTem, hasPresent)
+    const hasPresent = numberTem === selected;
+    return new DoorModel(numberTem, hasPresent);
+  });
+}
+
+export function updateDoors(doors: DoorModel[], doorModified: DoorModel): DoorModel[] {
+  return doors.map(currenDoor => {
+      const equalToModified = currenDoor.number === doorModified.number
+
+      if(equalToModified) {
+          return doorModified
+      } else {
+          return doorModified.isOpened ? currenDoor : currenDoor.unselect()
+      }
   })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import Door from "../components/Door";
 import Present from "../components/Present";
-import { createDoors } from "../functions/door";
+import { createDoors, updateDoors } from "../functions/door";
 import DoorModel from "../model/DoorModel";
 
 export default function Home() {
-  const [p1, setP1] = useState(new DoorModel(1))
-  const [p2, setP2] = useState(new DoorModel(2))
-  const [p3, setP3] = useState(new DoorModel(3))
+  const [doors, setDoors] = useState(createDoors(3, 2));
 
-console.log(createDoors(3, 2))
+  function renderDoors() {
+    return doors.map((door) => (
+      <Door
+        key={door.number}
+        value={door}
+        onChange={(newDoor) => setDoors(updateDoors(doors, newDoor))}
+      />
+    ));
+  }
 
-  return (
-    <div style={{display: 'flex'}}>
-      {/* <Present /> */}
-      <Door value={p1} onChange={n =>setP1(n)} />
-      <Door value={p2} onChange={n =>setP2(n)} />
-      <Door value={p3} onChange={n =>setP3(n)} />
-      
-    </div>
-  )
+  return <div style={{ display: "flex" }}>{renderDoors()}</div>;
 }
