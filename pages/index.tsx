c9bd5845e9498d92b57bf021d277b799f97abb26
Bruno Sajermann <sajermannbruno@gmail.com>
2022-11-03 07:02:37 -0300

    componente de jogo 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
 import { useState } from "react";
 import Door from "../components/Door";
-import Present from "../components/Present";
 import { createDoors, updateDoors } from "../functions/door";
-import DoorModel from "../model/DoorModel";
 
 export default function Home() {
-  const [doors, setDoors] = useState(createDoors(3, 2));
-
-  function renderDoors() {
-    return doors.map((door) => (
-      <Door
-        key={door.number}
-        value={door}
-        onChange={(newDoor) => setDoors(updateDoors(doors, newDoor))}
-      />
-    ));
-  }
-
-  return <div style={{ display: "flex" }}>{renderDoors()}</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <h1>Formulário de inicio</h1>
+    </div>
+  );
 }
